0ba2c31625c88f15ea314d4694822a92b5cda509
Karl Statz <karl.statz@gmail.com>
2025-03-06 17:01:29 -0600

    fix algorithms unit tests

--- algorithms/test/algorithms_test.cpp
+++ algorithms/test/algorithms_test.cpp
@@ -17,3 +17,16 @@ TEST(LINEAR_SEARCH_TEST, BasicAssertions) {
 
     EXPECT_EQ(idx_result, 1);
 }
+
+TEST(LINEAR_SEARCH_MISSING_TEST, BasicAssertions) {
+    int input[3] = {1, 2, 3};
+    Array* array = init(3, input);
+
+    const int idx_result = linear_search(array, 6);
+    EXPECT_EQ(idx_result, -1);
+}
+
+TEST(FACTORIAL_TEST, BasicAssertions) {
+    long result = factorial(5);
+    EXPECT_EQ(result, 120);
+}
