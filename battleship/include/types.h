79091162821d55dc7b0a068046c7d0781fec36ed
tathby <cross002@colum.edu>
2025-04-12 20:32:00 -0500

    Update types.h

--- battleship/include/types.h
+++ battleship/include/types.h
@@ -9,15 +9,4 @@ typedef unsigned const long uint64;
 typedef unsigned long mut_uint64;
 
 #endif  // TYPES_H_
-=======
-#ifndef TYPES_H_
-#define TYPES_H_
 
-// Custom Types
-typedef unsigned const int uint32;
-typedef unsigned int mut_uint32;
-typedef unsigned const long uint64;
-typedef unsigned long mut_uint64;
-
-#endif  // TYPES_H_
->>>>>>> 7dcff40e8bf12ff73921a872abd54fe043e9c661
