7e036b6e505f0e843d8c4e8faac3df3abaaaba02
tathby <cross002@colum.edu>
2025-04-12 20:32:40 -0500

    Update solver.h

--- battleship/include/solver.h
+++ battleship/include/solver.h
@@ -19,25 +19,4 @@ class Solver {
 };
 
 #endif  // SOLVER_H_
-=======
-#ifndef SOLVER_H_
-#define SOLVER_H_
 
-#include "board.h"
-#include "guesser.h"
-
-class Solver {
-   protected:
-    Guesser *guesser;
-
-    ReturnType guess(size_t x, size_t y, Board &board);
-
-   public:
-    Solver();
-    ~Solver();
-    virtual void solve(Board &board) = 0;
-    int score();
-};
-
-#endif  // SOLVER_H_
->>>>>>> 7dcff40e8bf12ff73921a872abd54fe043e9c661
