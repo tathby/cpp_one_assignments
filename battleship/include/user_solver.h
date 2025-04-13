0396236f81e1b7f41abd9803f725dc36eac8a64e
tathby <cross002@colum.edu>
2025-04-12 20:31:41 -0500

    Update user_solver.h

--- battleship/include/user_solver.h
+++ battleship/include/user_solver.h
@@ -13,16 +13,4 @@ class UserSolver : public Solver {
 };
 
 #endif  
-=======
-#ifndef USER_SOLVER_H_
-#define USER_SOLVER_H_
 
-#include "solver.h"
-
-class UserSolver : public Solver {
-   public:
-    void solve(Board &board) override;
-};
-
-#endif // USER_SOLVER_H_
->>>>>>> 7dcff40e8bf12ff73921a872abd54fe043e9c661
