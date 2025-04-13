957ddbf4f72ee1a86d974491b404460f22e2f1ff
tathby <cross002@colum.edu>
2025-04-12 20:36:17 -0500

    Update solver.cpp

--- battleship/src/solver.cpp
+++ battleship/src/solver.cpp
@@ -10,16 +10,4 @@ int Solver::score() { return this->guesser->getGuessCount(); }
 ReturnType Solver::guess(size_t x, size_t y, Board &board) {
     return this->guesser->guess(x, y, board);
 }
-=======
-#include "../include/solver.h"
 
-Solver::Solver() { this->guesser = new Guesser(); }
-
-Solver::~Solver() { delete guesser; }
-
-int Solver::score() { return this->guesser->getGuessCount(); }
-
-ReturnType Solver::guess(size_t x, size_t y, Board &board) {
-    return this->guesser->guess(x, y, board);
-}
->>>>>>> 7dcff40e8bf12ff73921a872abd54fe043e9c661
