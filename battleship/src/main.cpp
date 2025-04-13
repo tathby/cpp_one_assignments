98ab1395dd325d81a5ef58934ba36ec81a2ccd18
tathby <cross002@colum.edu>
2025-04-12 20:35:40 -0500

    Update main.cpp

--- battleship/src/main.cpp
+++ battleship/src/main.cpp
@@ -6,12 +6,4 @@
 #include "../include/user_solver.h"
 
 int main() {}
-=======
-#include <stdexcept>
 
-#include "../include/board.h"
-#include "../include/example_solver.h"
-#include "../include/user_solver.h"
-
-int main() {}
->>>>>>> 7dcff40e8bf12ff73921a872abd54fe043e9c661
