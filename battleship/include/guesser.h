981479ae0cb9d0651b1bfccadd7d84d93de3ba4a
tathby <cross002@colum.edu>
2025-04-12 20:32:56 -0500

    Update guesser.h

--- battleship/include/guesser.h
+++ battleship/include/guesser.h
@@ -21,27 +21,4 @@ class Guesser {
 };
 
 #endif  // GUESSER_H_
-=======
-#ifndef GUESSER_H_
-#define GUESSER_H_
 
-#include "board.h"
-#include "types.h"
-
-enum ReturnType { bad_guess, hit, miss, near_miss };
-
-struct GuessContext {
-    mut_uint32 guess_count;
-};
-
-class Guesser {
-    GuessContext ctx;
-    ReturnType check_coordinates(size_t x, size_t y, Board &board);
-
-   public:
-    ReturnType guess(size_t x, size_t y, Board &board);
-    int getGuessCount();
-};
-
-#endif  // GUESSER_H_
->>>>>>> 7dcff40e8bf12ff73921a872abd54fe043e9c661
