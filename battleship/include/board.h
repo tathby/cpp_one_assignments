70ca10df42e449d8f6c3e6b70d044eea75f66090
tathby <cross002@colum.edu>
2025-04-12 20:34:00 -0500

    Update board.h

--- battleship/include/board.h
+++ battleship/include/board.h
@@ -26,32 +26,4 @@ class Board {
 };
 
 #endif  // BOARD_H_
-=======
-#ifndef BOARD_H_
-#define BOARD_H_
 
-#include <cstddef>
-#include <vector>
-
-#include "types.h"
-
-class Board {
-   private:
-    std::vector<std::vector<mut_uint32> *> *grid;
-    void random_fill(uint32 ship_count);
-    mut_uint32 ship_count;
-    mut_uint32 x;
-    mut_uint32 y;
-
-   public:
-    Board(uint32 x, uint32 y, uint32 ship_count);
-    std::vector<std::vector<mut_uint32> *> *get_grid();
-    void print();
-    size_t getShipCount();
-    size_t getX();
-    size_t getY();
-    ~Board();
-};
-
-#endif  // BOARD_H_
->>>>>>> 7dcff40e8bf12ff73921a872abd54fe043e9c661
