550d4497094e1315e0821bc5399418af4469212c
tathby <cross002@colum.edu>
2025-04-12 20:33:40 -0500

    Update example_solver.h

--- battleship/include/example_solver.h
+++ battleship/include/example_solver.h
@@ -10,16 +10,4 @@ class ExampleSolver : public Solver {
 };
 
 #endif // EXAMPLE_SOLVER_H_
-=======
-#ifndef EXAMPLE_SOLVER_H_
-#define EXAMPLE_SOLVER_H_
 
-#include "solver.h"
-
-class ExampleSolver : public Solver {
-   public:
-    void solve(Board &board) override;
-};
-
-#endif // EXAMPLE_SOLVER_H_
->>>>>>> 7dcff40e8bf12ff73921a872abd54fe043e9c661
